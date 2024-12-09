76f0406eae8d344aef040ac3b20dca287df6656f
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-12-09 14:23:16 +0100

    displayed gameplay text

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const rulesText = document.querySelector("#rules-text");
 const playerScore = document.querySelector("#player-score");
 const computerScore = document.querySelector("#computer-score");
 
+const gameplayText = document.querySelector("#gameplay-text");
 const results = document.querySelector("#results");
 
 function displayRules() {
@@ -20,14 +21,43 @@ function displayRules() {
   }
 }
 
-function computerPlay() {
+function computerChoice() {
   const options = ["Rock", "Paper", "Scissors"];
-  const randomIndex = Math.floor(Math.random() * options.length);
-  return options[randomIndex];
+  let randomNumber = Math.floor(Math.random() * 3);
+  console.log(options[randomNumber]);
+  return options[randomNumber];
 }
 
-function playRock() {}
-function playPaper() {}
-function playScissors() {}
+function playRock() {
+  let opponentsChoice = computerChoice();
+  if (opponentsChoice === "Paper") {
+    gameplayText.innerText = `Computer has played: ${opponentsChoice}. You win!`;
+  } else if (opponentsChoice === "Scissors") {
+    gameplayText.innerText = `Computer has played: ${opponentsChoice}. You lose!`;
+  } else if (opponentsChoice === "Rock") {
+    gameplayText.innerText = `Computer has played: ${opponentsChoice}. It's a tie!`;
+  }
+}
+
+function playPaper() {
+  let opponentsChoice = computerChoice();
+  if (opponentsChoice === "Paper") {
+    gameplayText.innerText = `Computer has played: ${opponentsChoice}. It's a Tie!`;
+  } else if (opponentsChoice === "Scissors") {
+    gameplayText.innerText = `Computer has played: ${opponentsChoice}. You lose!`;
+  } else if (opponentsChoice === "Rock") {
+    gameplayText.innerText = `Computer has played: ${opponentsChoice}. You win!`;
+  }
+}
+function playScissors() {
+  let opponentsChoice = computerChoice();
+  if (opponentsChoice === "Paper") {
+    gameplayText.innerText = `Computer has played: ${opponentsChoice}. You win!`;
+  } else if (opponentsChoice === "Scissors") {
+    gameplayText.innerText = `Computer has played: ${opponentsChoice}. It's a tie!`;
+  } else if (opponentsChoice === "Rock") {
+    gameplayText.innerText = `Computer has played: ${opponentsChoice}. You lose!`;
+  }
+}
 
 rulesIcon.addEventListener("click", displayRules);
